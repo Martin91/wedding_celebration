c0b7763e2ab9ada27963bd32c5b962d240f6b88c
Martin <hongzeqin@gmail.com>
2013-10-14 00:46:13 +0800

    Initialize the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingCelebration::Application.config.secret_token = 'b49c443e3fe8d8b14c25541903c7090555dba59b1b1e165b2277f97047be2c1ebc582f7da1fc16a6315328d5aee45cecbf606d1784b409e27b74c0d6ee404a62'
