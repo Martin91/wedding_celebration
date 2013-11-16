e7befe503e25c5ad070afd1baaf061cbefad79c4
Martin <hongzeqin@gmail.com>
2013-11-16 11:15:18 +0800

    customize the index and show page

--- app/admin/blessings.rb
+++ app/admin/blessings.rb
@@ -3,6 +3,27 @@
 ActiveAdmin.register Blessing do
   menu label: "祝福"
 
+  index do
+    selectable_column
+    column "小老", :editor
+    column "祝福" do |blessing|
+      raw blessing.content
+    end
+    column "留言时间", :created_at
+    column "更新时间", :updated_at
+
+    default_actions
+  end
+
+  show do
+    attributes_table do
+      row :editor
+      row :content do
+        raw blessing.content
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :editor, label: "留言人姓名"
