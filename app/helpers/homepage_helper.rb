17ff9d20cd175d688f240b3e7573ea33b951dbef
Martin <hongzeqin@gmail.com>
2013-11-16 21:09:32 +0800

    Complete the story

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -2,4 +2,8 @@ module HomepageHelper
   def render_homepage_partial(partial_name)
     render "home/home/#{partial_name}"
   end
+
+  def screen_shots(image_name)
+    "screenshots/#{image_name}.png"
+  end
 end
